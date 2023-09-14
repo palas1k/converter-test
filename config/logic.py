3085087b235e0d545b50de6f0d99810ec071bfc5
Nail Fatkhutdinov <liserg008@gmail.com>
2023-09-14 22:02:19 +0300

    converter logic

---
+++ config/logic.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+
+def get_currence_price(ticker):
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    data = requests.get(url).text
+    data_json = json.loads(data)
+    currency = data_json["Valute"][ticker]["Value"]
+    return currency
+
+
+def get_course(cur1, cur2, value:int):
+    a = get_currence_price(cur1)
+    b = get_currence_price(cur2)
+    return print(round((a*value / b), 2), cur2)
+
+get_course('USD', 'EUR', )
